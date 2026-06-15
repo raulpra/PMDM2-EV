18f387fb22437ee51c67cce1efda49ff4fa79b3b
raulpra <raulprada@gmail.com>
2026-06-15 13:11:15 +0200

    feat: añadir clase Constants con variables globales

---
+++ core/src/main/java/com/juego/util/Constants.java
@@ -0,0 +1,13 @@
+package com.juego.util;
+
+/**
+ * Clase para almacenar todas las variables globales y constantes del juego.
+ */
+public class Constants {
+
+    // Dimensiones de la pantalla
+    public static final int APP_WIDTH = 800;
+    public static final int APP_HEIGHT = 480;
+    // Pixels Per Meter (necesario para las físicas de Box2D)
+    public static final float PPM = 100f;
+}
