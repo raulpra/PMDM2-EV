b4d46caad901a0223937a0a6feef005417f421e3
raulpra <raulprada@gmail.com>
2026-06-15 19:23:27 +0200

    Preparar Player para manejar animaciones y estados

--- core/src/main/java/com/juego/domain/Player.java
+++ core/src/main/java/com/juego/domain/Player.java
@@ -10,13 +10,16 @@ public class Player {
     private final Vector2 position;
     private final Vector2 velocity;
     private final Rectangle bounds;
+    private float stateTimer; // Cronómetro para saber qué fotograma pintar
+    private boolean facingRight; // ¿Mira a la derecha?
 
+    // Modifica el constructor:
     public Player(float startX, float startY) {
         position = new Vector2(startX, startY);
         velocity = new Vector2(0, 0);
-
-        // Asumimos que nuestro personaje mide 16 de ancho y 24 de alto
-        bounds = new Rectangle(startX, startY, 16, 24);
+        bounds = new Rectangle(startX, startY, 16, 16); // El OnionLad mide 16x16
+        stateTimer = 0;
+        facingRight = true;
     }
 
     public void update(float delta) {
@@ -25,9 +28,19 @@ public class Player {
 
         // Actualizamos la caja invisible para que siga al dibujo del jugador
         bounds.setPosition(position.x, position.y);
+        // Sumamos el tiempo
+        stateTimer += delta;
+        // Actualizamos hacia dónde mira según su velocidad
+        if (velocity.x > 0 && !facingRight) {
+            facingRight = true;
+        } else if (velocity.x < 0 && facingRight) {
+            facingRight = false;
+        }
     }
 
     public Vector2 getPosition() { return position; }
     public Vector2 getVelocity() { return velocity; }
     public Rectangle getBounds() { return bounds; }
+    public float getStateTimer() { return stateTimer; }
+    public boolean isFacingRight() { return facingRight; }
 }
