a3a09398a60cd65c4ae40477b572d7ce8f2f1044
raulpra <raulprada@gmail.com>
2026-06-15 14:38:15 +0200

    feat: crear el primer mapa nivel1.tmx con Tiled usando el tileset

---
+++ assets/maps/Extra_plants (16 x 16).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Extra_plants (16 x 16)" tilewidth="16" tileheight="16" tilecount="50" columns="10">
+ <image source="../images/Extra_plants (16 x 16).png" width="160" height="80"/>
+</tileset>
