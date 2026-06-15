2516b351bec3b2d2618380231a552c2297202967
raulpra <raulprada@gmail.com>
2026-06-15 13:21:13 +0200

    fix: actualizar ruta de la clase principal en build.gradle

--- lwjgl3/src/main/java/com/juego/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/juego/lwjgl3/Lwjgl3Launcher.java
@@ -15,7 +15,7 @@ public class Lwjgl3Launcher {
     }
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("Mi Juego de Plataformas"); // Pon el título que quieras
+        configuration.setTitle("Mi Juego de Plataformas");
         configuration.useVsync(true);
         configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
         configuration.setWindowedMode(800, 480);
